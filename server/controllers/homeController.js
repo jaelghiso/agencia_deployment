7c37b80b095e77196b073f2ac8c67649becd51fd
jaelghiso <jael.ghiso@gmail.com>
2020-02-09 17:19:32 -0300

    first commit

---
+++ server/controllers/homeController.js
@@ -0,0 +1,15 @@
+const Viaje = require('../models/viajes');
+const Testimonial = require('../models/Testimoniales');
+
+exports.consultasHomePage = async (req,res) =>{
+
+    const viajes =  await Viaje.findAll({ limit: 3 })
+    const testimoniales = await Testimonial.findAll({ limit: 3 })
+    
+    res.render('index', {
+        pagina: 'Próximos Viajes',
+        clase: 'home',
+        viajes,
+        testimoniales
+    })
+}
---
+++ server/controllers/testimonialesController.js
@@ -0,0 +1,42 @@
+const Testimonial = require('../models/Testimoniales');
+
+exports.mostrarTestimoniales = async (req, res) => {
+    const testimoniales = await Testimonial.findAll()
+    res.render('testimoniales', {
+        pagina: 'Testimoniales',
+        testimoniales: testimoniales
+    })
+}
+
+exports.crearTestimonial = async (req, res) =>{
+    let { name, email, message } = req.body;
+
+    let errores = [];
+    if(!name){
+        errores.push({'mensaje': 'Agrega tu nombre'})
+    }
+    if(!email){
+        errores.push({'mensaje': 'Agrega tu email'})
+    }if(!message){
+        errores.push({'mensaje': 'Escribe tu testimonio'})
+    }
+
+    if(errores.length > 0){
+        const testimoniales =  await Testimonial.findAll()
+        res.render('testimoniales', {
+            errores,
+            name,
+            email,
+            message,
+            pagina: 'Testimoniales',
+            testimoniales
+        })
+    } else{
+        Testimonial.create({
+            name,
+            email,
+            message
+        }).then(testimonial => res.redirect('/testimoniales'))
+          .catch(error => console.log(error));
+    }
+}
